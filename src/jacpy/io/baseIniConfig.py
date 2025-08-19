57684de07419ad401b1e18bf9512652cae463501
Alberto Anguita <alberto.anguita@gmail.com>
2025-08-19 17:53:11 +0200

    Add config methods

--- src/jacpy/io/baseIniConfig.py
+++ src/jacpy/io/baseIniConfig.py
@@ -12,11 +12,20 @@ class BaseIniConfig(Singleton):
             self.__config.read(path)
             self._initialized = True
 
-    def _get_str_value(self, section: str, key) -> str:
+    def _get_str_value(self, section: str, key: str) -> str:
         return self.__config[section][key]
 
-    def _get_int_value(self, section: str, key) -> int:
+    def _get_int_value(self, section: str, key: str) -> int:
         return int(self.__config[section][key])
+
+    def _get_float_value(self, section: str, key: str) -> float:
+        return float(self.__config[section][key])
+
+    def _get_bool_value(self, section: str, key: str) -> bool:
+        return self.__config[section][key].lower() == 'true'
+
+    def _set_value(self, section: str, key: str, value) -> None:
+        self.__config[section][key] = value
 
     def items(self):
         return self.__config.items()
